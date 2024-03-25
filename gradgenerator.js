e8ae8f19c01c0a685881d83195d8ba76e29b7300
its-mst <stellaoluoch231@gmail.com>
2024-03-25 08:12:23 -0400

    Initial commit

---
+++ gradgenerator.js
@@ -0,0 +1,17 @@
+let studentMarks=parseInt(window.prompt("Enter your name: "))
+if(studentMarks>79){
+    console.log("A");
+}
+else if(studentMarks>59){
+    console.log("B");
+}
+else if(studentMarks>49){
+    console.log("C");
+}
+else if(studentMarks>39){
+    console.log("D");
+}
+else{
+    console.log("E");
+}
+
